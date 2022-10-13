02ed92eb0a1e5f117a6357e1bb54b0294775f0c7
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-10-13 18:24:56 -0500

    Adding filesystem method to read a document on a certain path

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+fs.readFile('documents/music-sources.md', 'utf-8', (error, data)=> {
+    if (!error) {
+        console.log(data);
+    } else console.log(error);
+})
