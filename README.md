ead3d64b62cea29f0c2548aeea9938b19acaa47d
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-10-17 19:38:20 -0500

    Adding functions to transform a relative path

--- README.md
+++ README.md
@@ -507,7 +507,7 @@ si tienes dudas existenciales con respecto a estas decisiones. No existe una
 
 ### General
 
-* [ ] Puede instalarse via `npm install --global <github-user>/md-links`
+* [ ] Puede instalarse via `npm install --location=global vehemencia/md-links`
 
 ### `README.md`
 
@@ -544,44 +544,6 @@ esquinas o bordes del rompecabezas/puzzle sin saber necesariamente
 cómo encajarán al final. Déjate llevar y explora.
 
 Estas son algunas sugerencias:
-
-### Empieza con un diagrama de flujo
-
-Este proyecto es distinto de los que has venido trabajando hasta ahora
-dado que no hay una interfaz web, todo se desarrollará en tu editor y
-consola/terminal.
-
-Es por ello que, para visualizar mejor lo que tendrás que hacer
-y planificar tus tareas y objetivos, es recomendable hacer un
-`diagrama de flujo`.
-
-Si nunca has hecho un diagrama de flujo revisa este [recurso](https://www.youtube.com/watch?v=Lub5qOmY4JQ).
-
-Una alternativa al diagrama de flujo puede ser el `pseudocódigo`.
-
-### Planificación
-
-En este proyecto te recomendamos usar la herramienta de planificación
-y organización de GitHub llamada **Github Projects**.
-
-Mediante **issues** y **milestones** podrás organizar y planificar
-tareas y objetivos concretos.
-
-Tomando en consideración los **entregables** del proyecto, el
-[9. Checklist](#9-checklist) y los **pasos** que definiste en tu
-`diagrama de flujo`, crea tu planificación en GitHub Projects.
-
-### Antes de codear
-
-En esta ocasión estarás trabajando en **NodeJS**, asegúrate
-de saber para qué sirve y sus consideraciones.
-
-En particular, deberás decidir desde un comienzo si usarás
-`ES Modules`, es decir, **import/export**, ó, por el contrario,
-`CommonJS Modules`, es decir, **require/module.exports**.
-
-Asegurate de tener clara esta decisión desde un inicio para
-que no encuentres problemas más adelante.
 
 ### Lee un archivo
 
--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const process = require('process');
 
 // Reading content from a file
-fs.readFile('documents/music-sources.md', 'utf-8', (error, data) => {
-    if (!error) {
-        console.log(data);
-    } else console.log(error);
-});
+// fs.readFile('documents/music-sources.md', 'utf-8', (error, data) => {
+//     if (!error) {
+//         console.log(data);
+//     } else console.log(error);
+// });
 
 // Determining the extension from a document
 // console.log(path.extname('documents/music-sources.md'));
@@ -19,9 +19,46 @@ fs.readFile('documents/music-sources.md', 'utf-8', (error, data) => {
 //         if (path.extname(document) == '.md') {
 //             console.log(document);
 //         };
-//     })
+//     });
 // });
 
-// function mdLinks(path, options){
 
-// }
+// Knowing if a path is absolute or relative,
+// console.log(path.isAbsolute('/index.js'));
+
+// The next function will verify if a path is absolute or relative; in case that it isn't absolute, it will convert it into one
+function verifyTypeOfPath(givenPath) {
+    let definePathType = path.isAbsolute(givenPath);
+    console.log(path.isAbsolute(givenPath))
+    if (definePathType === false) {
+        let conversionToRelativePath = path.resolve();
+        console.log(path.basename(conversionToRelativePath), path.isAbsolute(conversionToRelativePath));
+    }
+};
+
+// This function will verify if it is a folder or a file
+function folderOrFile(givenPath) {
+    if (fs.lstatSync(givenPath).isDirectory() === true) {
+        console.log('Sí es una carpeta, ahora enumeraré los archivos y otras carpetas')
+        fs.readdir(givenPath, (error, files) => {
+            console.log(files);
+            files.forEach(document => {
+                if (path.extname(document) === '.md') {
+                    console.log(document);
+                }
+                if (path.extname(document) === '') {
+                    console.log(document);
+                };
+            });
+        });
+    } else {
+        console.log('No es una carpeta');
+    }
+}
+
+function mdLinks(givenPath) {
+    verifyTypeOfPath(givenPath);
+    folderOrFile(givenPath)
+};
+
+mdLinks('documents/music-sources.md');
