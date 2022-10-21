6b4843c80cc116a70183ea5209b275038904e63f
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-10-20 19:38:08 -0500

    Fixed converting a relative path into absolute, error now returns a message

--- index.js
+++ index.js
@@ -1,64 +1,29 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
-
-// Reading content from a file
-// fs.readFile('documents/music-sources.md', 'utf-8', (error, data) => {
-//     if (!error) {
-//         console.log(data);
-//     } else console.log(error);
-// });
-
-// Determining the extension from a document
-// console.log(path.extname('documents/music-sources.md'));
-
-// Reading content inside a folder
-// fs.readdir('C:/Users/Maria A. Ramirez/Documents/Laboratoria/Proyecto 4/CDMX013-md-links/documents', (error, files) => {
-//     console.log(files);
-//     files.forEach(document => {
-//         if (path.extname(document) == '.md') {
-//             console.log(document);
-//         };
-//     });
-// });
-
-
-// Knowing if a path is absolute or relative,
-// console.log(path.isAbsolute('/index.js'));
-
-// The next function will verify if a path is absolute or relative; in case that it isn't absolute, it will convert it into one
-function verifyTypeOfPath(givenPath) {
-    let definePathType = path.isAbsolute(givenPath);
-    console.log(path.isAbsolute(givenPath))
-    if (definePathType === false) {
-        let conversionToRelativePath = path.resolve();
-        console.log(path.basename(conversionToRelativePath), path.isAbsolute(conversionToRelativePath));
-    }
-};
-
-// This function will verify if it is a folder or a file
-function folderOrFile(givenPath) {
-    if (fs.lstatSync(givenPath).isDirectory() === true) {
-        console.log('Sí es una carpeta, ahora enumeraré los archivos y otras carpetas')
-        fs.readdir(givenPath, (error, files) => {
-            console.log(files);
-            files.forEach(document => {
-                if (path.extname(document) === '.md') {
-                    console.log(document);
-                }
-                if (path.extname(document) === '') {
-                    console.log(document);
-                };
-            });
-        });
-    } else {
-        console.log('No es una carpeta');
-    }
-}
 
 function mdLinks(givenPath) {
-    verifyTypeOfPath(givenPath);
-    folderOrFile(givenPath)
+    try {
+        if (path.isAbsolute(givenPath) === false) {
+            const transformIntoAbsolutePath = path.resolve(givenPath);
+            givenPath = transformIntoAbsolutePath;
+        };
+        if (fs.lstatSync(givenPath).isDirectory() === true) {
+            console.log('This is a folder')
+        } else {
+            console.log('This is a file');
+            if (path.extname(givenPath) === '.md') {
+                fs.readFile(givenPath, 'utf-8', (error, data) => {
+                    if (!error) {
+                        console.log(data);
+                    } else console.log(error);
+                });
+            } else {
+                console.log('This is not a .md file, please try again')
+            };
+        };
+    } catch (error) {
+        console.log(`Something happened, but it isn't your fault, the document you are looking for is missing or the path doesn't exist`);
+    }
 };
 
-mdLinks('documents/music-sources.md');
+mdLinks('documentss');
