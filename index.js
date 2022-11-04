333ba2376fa0b53e329873ff382c91a5dba94fc0
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-11-04 12:47:40 -0600

    Fix: link validation function now includes new properties

--- index.js
+++ index.js
@@ -3,52 +3,64 @@ const path = require('path');
 const relativeToAbsolute = require('./modularization/relative-to-absolute-path.js');
 const isItMarkdown = require('./modularization/verify-markdown-file.js');
 const obtainLinks = require('./modularization/obtain-links.js');
-//const validateLinks = require('./modularization/validate-links.js');
+//const validateLink = require('./modularization/validate-links.js');
 const axios = require('axios');
 
-function mdLinks(givenPath, options) {
-    const mdLinksPromise = new Promise((resolve, reject) => {
-        const absolutePath = relativeToAbsolute(givenPath);
-        if (fs.lstatSync(absolutePath).isDirectory() === true) {
-            resolve('This is a folder, try with a .md document');
-        } else if (fs.lstatSync(absolutePath).isDirectory() === false) {
-            if (options === '{validate: false}' || options === undefined) {
-                if (isItMarkdown(absolutePath) === true) {
-                    resolve(obtainLinks(givenPath))
-                } else {
-                    reject('This is not a .md file')
-                }
-            } else if (options === '{validate: true}') {
-               resolve('Working on it... beep')
-            } else {
-                reject('Something happened, but it is not your fault, try again')
-            }
-        }
-    })
-    return mdLinksPromise;
-}
+// function mdLinks(givenPath, options) {
+//     const mdLinksPromise = new Promise((resolve, reject) => {
+//         const absolutePath = relativeToAbsolute(givenPath);
+//         if (fs.lstatSync(absolutePath).isDirectory() === true) {
+//             resolve('This is a folder, try with a .md document');
+//         } else if (fs.lstatSync(absolutePath).isDirectory() === false) {
+//             if (options === '{validate: false}' || options === undefined) {
+//                 if (isItMarkdown(absolutePath) === true) {
+//                     resolve(obtainLinks(givenPath))
+//                 } else {
+//                     reject('This is not a .md file')
+//                 }
+//             } else if (options === '{validate: true}') {
+//                 resolve('A :c')
+//             } else {
+//                 reject('Something happened, but it is not your fault, try again')
+//             }
+//         }
+//     })
+//     return mdLinksPromise;
+// }
 
 // const myPromise = mdLinks('documents/without-links.md', '{validate: true}');
 // myPromise.then(console.log).catch(console.log)
 
-// function validateLinks(givenPath) {
-//     let arrayWithObjects = obtainLinks(givenPath);
-//     const linksInside = arrayWithObjects.map(linkProperty => linkProperty.href);
-//     linksInside.forEach(element => {
-//         const request = axios.get(element);
-//         return request
-//             .then(response => console.log(response.status))
-//             .catch(error => console.log(error.response.status))
-//     });
-// }
-
-// function validateLink(link){
-//     const request = axios.get(link.href);
+// function validateLinkPrueba(link){
+//     const request = axios.get(link);
 //     return request
-//     .then(response => 
-//         {console.log(response)}
-//         )
+//     .then(response => console.log(response.status))
+//     .catch(error => console.log(error.response.message))
 // }
 
+function validateLinks(givenPath) {
+    let arrayWithObjects = obtainLinks(givenPath);
+    const linksInside = arrayWithObjects.forEach(linkProperty => {
+        const request = axios.get(linkProperty.href);
+        return request
+            .then((response) => {
+                let arregloParaDevolver = {
+                    ...linkProperty,
+                    status: response.status,
+                    message: 'ok'
+                };
+                console.log(arregloParaDevolver)
+            })
+            .catch((error) => {
+                let arregloConError = {
+                    ...linkProperty,
+                    status: error.response.status,
+                    message: 'fail'
+                };
+                console.log(arregloConError)
+            })
+    })
+}
 
-mdLinks('documents/music-sources-1.md', '{validate: true}').then(console.log).catch(console.log);
+validateLinks('documents/music-sources-1.md')
+//mdLinks('documents/music-sources-1.md', '{validate: true}').then(console.log).catch(console.log);
--- modularization/validate-links.js
+++ modularization/validate-links.js
@@ -1,13 +1,14 @@
 // const axios = require('axios');
-// const obtainLinks = require('./obtain-links.js');
+// // const obtainLinks = require('./obtain-links.js');
 
-// function validateLinks(givenPath) {
-//     const arrayOfLinks = obtainLinks(givenPath);
-//     const httpRequest = axios.get(arrayOfLinks.href);
-//      return httpRequest
-//      .then(response=>console.log(response))
-//  }
+// function validateLink(link){
+//     const request = axios.get(link.href);
+//     return request
+//     .then(response => 
+//         {console.log(response)}
+//         )
+// }
 
-// validateLinks('documents/music-sources.md')
+// // validateLinks('documents/music-sources.md')
 
-// module.exports = validateLinks;
+// module.exports = validateLink;
