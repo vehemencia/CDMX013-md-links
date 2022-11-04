696ef36b69915ad9ed9e2ed6126acf067948bdf5
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-11-03 18:33:44 -0600

    Update: Working on individual HTTP request

--- documents/music-sources-1.md
+++ documents/music-sources-1.md
@@ -1,2 +1,3 @@
 [Tone Glow is now a newsletter! This site will no longer be updated, but will stay up for posterity.](https://www.toneglow.net/)
 [Bandcamp Daily](https://daily.bandcamp.com/) 
+[uwu](https://www.lego.com/es-mx/404)
--- index.js
+++ index.js
@@ -6,39 +6,49 @@ const obtainLinks = require('./modularization/obtain-links.js');
 //const validateLinks = require('./modularization/validate-links.js');
 const axios = require('axios');
 
-// function mdLinks(givenPath, options) {
-//     const mdLinksPromise = new Promise((resolve, reject) => {
-//         const absolutePath = relativeToAbsolute(givenPath);
-//         if (fs.lstatSync(absolutePath).isDirectory() === true) {
-//             resolve('This is a folder, try with a .md document');
-//         } else if (fs.lstatSync(absolutePath).isDirectory() === false) {
-//             if (options === '{validate: false}' || options === undefined) {
-//                 if (isItMarkdown(absolutePath) === true) {
-//                     resolve(obtainLinks(givenPath))
-//                 } else {
-//                     reject('This is not a .md file')
-//                 }
-//             } else if (options === '{validate: true}') {
-//                 resolve(validateLinks(givenPath))
-//             } else {
-//                 reject('Something happened, but it is not your fault, try again')
-//             }
-//         }
-//     })
-//     return mdLinksPromise;
-// }
+function mdLinks(givenPath, options) {
+    const mdLinksPromise = new Promise((resolve, reject) => {
+        const absolutePath = relativeToAbsolute(givenPath);
+        if (fs.lstatSync(absolutePath).isDirectory() === true) {
+            resolve('This is a folder, try with a .md document');
+        } else if (fs.lstatSync(absolutePath).isDirectory() === false) {
+            if (options === '{validate: false}' || options === undefined) {
+                if (isItMarkdown(absolutePath) === true) {
+                    resolve(obtainLinks(givenPath))
+                } else {
+                    reject('This is not a .md file')
+                }
+            } else if (options === '{validate: true}') {
+               resolve('Working on it... beep')
+            } else {
+                reject('Something happened, but it is not your fault, try again')
+            }
+        }
+    })
+    return mdLinksPromise;
+}
 
 // const myPromise = mdLinks('documents/without-links.md', '{validate: true}');
 // myPromise.then(console.log).catch(console.log)
 
-function validateLinks(givenPath) {
+// function validateLinks(givenPath) {
+//     let arrayWithObjects = obtainLinks(givenPath);
+//     const linksInside = arrayWithObjects.map(linkProperty => linkProperty.href);
+//     linksInside.forEach(element => {
+//         const request = axios.get(element);
+//         return request
+//             .then(response => console.log(response.status))
+//             .catch(error => console.log(error.response.status))
+//     });
+// }
 
-    const linksInside = obtainLinks(givenPath).map(linkProperty => linkProperty.href);
-    linksInside.forEach(element => {
-        const request = axios.get(element);
-        return request
-        .then(response => console.log(response.status, response.statusText));
-    });
-}
+// function validateLink(link){
+//     const request = axios.get(link.href);
+//     return request
+//     .then(response => 
+//         {console.log(response)}
+//         )
+// }
 
-validateLinks('documents/music-sources-1.md')
+
+mdLinks('documents/music-sources-1.md', '{validate: true}').then(console.log).catch(console.log);
